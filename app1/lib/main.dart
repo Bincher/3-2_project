d4ed330bb8e60b07321825c8c88be016ab909797
seongbin <catsb21@gmail.com>
2023-10-04 17:27:10 +0900

    텍스트 필드

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,44 +1,71 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: const Text('Test'),
+              title: Text('Test'),
             ),
-            body: const TestScreen()));
+            body: TestScreen()));
   }
 }
 
 class TestScreen extends StatefulWidget {
-  const TestScreen({super.key});
-
   @override
-  MyFormState createState() => MyFormState();
+  TextState createState() => TextState();
 }
 
-class MyFormState extends State<TestScreen> {
+class TextState extends State<TestScreen> {
+  final controller = TextEditingController();
+  int textCounter = 0;
+
+  _printValue() {
+    print("_printValue(): ${controller.text}");
+    setState(() {
+      textCounter = controller.text.length;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_printValue);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    print("build....");
+    return Column(
       children: [
-        Spacer(),
-        Text('Form Test'),
-        SizedBox(height: 20),
-        Text('Form Test'),
-        SizedBox(height: 40),
-        Text('Form Test'),
-        Spacer(),
+        Text('TextField Test'),
+        TextField(
+          style: TextStyle(fontSize: 15.0),
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: 'Data',
+            prefixIcon: Icon(Icons.input),
+            border: OutlineInputBorder(),
+            hintText: "Hint Text",
+            helperText: "데이터를 입력하세요.",
+            counterText: "$textCounter characters",
+          ),
+          textInputAction: TextInputAction.search,
+          keyboardType: TextInputType.emailAddress,
+          minLines: 5,
+          maxLines: 5,
+        ),
       ],
     );
   }
