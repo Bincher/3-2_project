610ce9d0507cb60be34f6832993926028c535a76
seongbin <catsb21@gmail.com>
2023-10-05 11:30:31 +0900

    리스트뷰 활용

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,85 +1,51 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class User {
+  String name;
+  String phone;
+  String email;
+  User(this.name, this.phone, this.email);
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  List<User> users = [
+    User('홍길동','0100001','a@a.com'), User('김길동','0100002','b@a.com'),
+    User('이길동','0100003','c@a.com'), User('박길동','0100004','d@a.com'),
+    User('홍길동','0100001','a@a.com'), User('김길동','0100002','b@a.com'),
+    User('이길동','0100003','c@a.com'), User('박길동','0100004','d@a.com'),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Test'),
-            ),
-            body: const TestScreen()));
-  }
-}
-
-class TestScreen extends StatefulWidget {
-  const TestScreen({super.key});
-
-  @override
-  MyFormState createState() => MyFormState();
-}
-
-class MyFormState extends State<TestScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String? firstName;
-  String? lastName;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
-          const Text('Form Test'),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'FirstName'),
-                  validator: (value) {
-                    if (value?.isEmpty ?? false) {
-                      return 'Please enter first name';
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    firstName = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'LastName'),
-                  validator: (value) {
-                    if (value?.isEmpty ?? false) {
-                      return 'Please enter last name';
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    lastName = value;
-                  },
-                ),
-              ],
-            ),
+          appBar: AppBar(
+            title: Text('Test'),
           ),
-          ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState?.validate() ?? false) {
-                  _formKey.currentState?.save();
-                  print('firstName: $firstName, lastName : $lastName');
-                }
-              },
-              child: const Text('submit')),
-        ],
-      ),
+          body: ListView.separated(
+            itemCount: users.length,
+            itemBuilder: (context, index){
+              return ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage('images/big.jpeg'),
+                ),
+                title: Text(users[index].name),
+                subtitle: Text(users[index].phone),
+                trailing: Icon(Icons.more_vert),
+                onTap: (){
+                  print(users[index].name);
+                },
+              );
+            },
+            separatorBuilder: (context, index){
+              return Divider(height: 2, color: Colors.black,);
+            },
+          ),
+        )
     );
   }
 }
