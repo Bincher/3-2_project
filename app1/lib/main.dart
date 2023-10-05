5a78ee3b06ba29fd88f2c14bfdb446bf764239a3
seongbin <catsb21@gmail.com>
2023-10-05 11:33:58 +0900

    페이지 뷰 활용 - 움직임 X

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -4,48 +4,45 @@ void main() {
   runApp(MyApp());
 }
 
-class User {
-  String name;
-  String phone;
-  String email;
-  User(this.name, this.phone, this.email);
-}
-
 class MyApp extends StatelessWidget {
-  List<User> users = [
-    User('홍길동','0100001','a@a.com'), User('김길동','0100002','b@a.com'),
-    User('이길동','0100003','c@a.com'), User('박길동','0100004','d@a.com'),
-    User('홍길동','0100001','a@a.com'), User('김길동','0100002','b@a.com'),
-    User('이길동','0100003','c@a.com'), User('박길동','0100004','d@a.com'),
-  ];
+  PageController controller =
+      PageController(initialPage: 1, viewportFraction: 0.8);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Test'),
-          ),
-          body: ListView.separated(
-            itemCount: users.length,
-            itemBuilder: (context, index){
-              return ListTile(
-                leading: CircleAvatar(
-                  radius: 25,
-                  backgroundImage: AssetImage('images/big.jpeg'),
+            appBar: AppBar(
+              title: Text('Test'),
+            ),
+            body: PageView(
+              controller: controller,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(20),
+                  color: Colors.red,
+                  child: Center(
+                    child: Text('OnePage',
+                        style: TextStyle(color: Colors.white, fontSize: 30)),
+                  ),
                 ),
-                title: Text(users[index].name),
-                subtitle: Text(users[index].phone),
-                trailing: Icon(Icons.more_vert),
-                onTap: (){
-                  print(users[index].name);
-                },
-              );
-            },
-            separatorBuilder: (context, index){
-              return Divider(height: 2, color: Colors.black,);
-            },
-          ),
-        )
-    );
+                Container(
+                  margin: EdgeInsets.all(20),
+                  color: Colors.green,
+                  child: Center(
+                    child: Text('TwoPage',
+                        style: TextStyle(color: Colors.white, fontSize: 30)),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text('ThreePage',
+                        style: TextStyle(color: Colors.white, fontSize: 30)),
+                  ),
+                ),
+              ],
+            )));
   }
 }
