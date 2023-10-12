ade667518108ac374a0d4b5cdb43981abdd5856e
seongbin <catsb21@gmail.com>
2023-10-12 10:24:51 +0900

    스캐폴드 활용하기

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,125 +1,128 @@
-// main.dart
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      // Remove the debug banner
-      debugShowCheckedModeBanner: false,
-      title: 'Kindacode.com',
-      home: HomePage(),
-    );
-  }
+  MyAppState createState() => MyAppState();
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  // This holds a list of fiction users
-  // You can use data fetched from a database or a server as well
-  final List<Map<String, dynamic>> _allUsers = [
-    {"id": 1, "name": "Andy", "age": 29},
-    {"id": 2, "name": "Aragon", "age": 40},
-    {"id": 3, "name": "Bob", "age": 5},
-    {"id": 4, "name": "Barbara", "age": 35},
-    {"id": 5, "name": "Candy", "age": 21},
-    {"id": 6, "name": "Colin", "age": 55},
-    {"id": 7, "name": "Audra", "age": 30},
-    {"id": 8, "name": "Banana", "age": 14},
-    {"id": 9, "name": "Caversky", "age": 100},
-    {"id": 10, "name": "Becky", "age": 32},
+  final List<Widget> _widgetOptions = <Widget>[
+    const Text(
+      'First Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      'Second Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      'Third Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      'Fourth Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
   ];
 
-  // This list holds the data for the list view
-  List<Map<String, dynamic>> _foundUsers = [];
-  @override
-  initState() {
-    // at the beginning, all users are shown
-    _foundUsers = _allUsers;
-    super.initState();
-  }
-
-  // This function is called whenever the text field changes
-  void _runFilter(String enteredKeyword) {
-    List<Map<String, dynamic>> results = [];
-    if (enteredKeyword.isEmpty) {
-      // if the search field is empty or only contains white-space, we'll display all users
-      results = _allUsers;
-    } else {
-      results = _allUsers
-          .where((user) =>
-              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
-          .toList();
-      // we use the toLowerCase() method to make it case-insensitive
-    }
-
-    // Refresh the UI
+  void _onItemTapped(int index) {
     setState(() {
-      _foundUsers = results;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Kindacode.com'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(48.0),
+                child: Theme(
+                  data: Theme.of(context).copyWith(),
+                  child: Container(
+                      height: 48.0,
+                      alignment: Alignment.center,
+                      child: const Text('AppBar Bottom Text')),
+                ),
+              ),
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/big.jpeg'),
+                        fit: BoxFit.fill)),
+              ),
+              title: const Text('AppBar Title'),
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.add_alert),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.phone),
+                  onPressed: () {},
+                ),
+              ]),
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            //fixed, shifting
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'First',
+                  backgroundColor: Colors.green),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.business),
+                  label: 'Second',
+                  backgroundColor: Colors.red),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: 'Third',
+                  backgroundColor: Colors.purple),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: 'Fourth',
+                  backgroundColor: Colors.pink)
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: _onItemTapped,
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text('Drawer Header'),
+                ),
+                ListTile(
+                  title: const Text('Item 1'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () {},
+                ),
+              ],
             ),
-            TextField(
-              onChanged: (value) => _runFilter(value),
-              decoration: const InputDecoration(
-                  labelText: 'Search', suffixIcon: Icon(Icons.search)),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: _foundUsers.isNotEmpty
-                  ? ListView.builder(
-                      itemCount: _foundUsers.length,
-                      itemBuilder: (context, index) => Card(
-                        key: ValueKey(_foundUsers[index]["id"]),
-                        color: Colors.amberAccent,
-                        elevation: 4,
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        child: ListTile(
-                          leading: Text(
-                            _foundUsers[index]["id"].toString(),
-                            style: const TextStyle(fontSize: 24),
-                          ),
-                          title: Text(_foundUsers[index]['name']),
-                          subtitle: Text(
-                              '${_foundUsers[index]["age"].toString()} years old'),
-                        ),
-                      ),
-                    )
-                  : const Text(
-                      'No results found',
-                      style: TextStyle(fontSize: 24),
-                    ),
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
