a77b6d5f9a4567412bc214c95f6bb7c1aedb7258
seongbin <catsb21@gmail.com>
2023-10-12 10:40:35 +0900

    내비게이션(라우트 이름)

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 void main() {
   runApp(Nav2App());
 }
@@ -9,7 +8,10 @@ class Nav2App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/details': (context) => DetailScreen(),
+      },
     );
   }
 }
@@ -21,13 +23,12 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Center(
         child: ElevatedButton(
+          //FlatButton
           child: Text('View Details'),
           onPressed: () {
-            Navigator.push(
+            Navigator.pushNamed(
               context,
-              MaterialPageRoute(builder: (context) {
-                return DetailScreen();
-              }),
+              '/details',
             );
           },
         ),
@@ -37,143 +38,20 @@ class HomeScreen extends StatelessWidget {
 }
 
 class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: ElevatedButton(
-          child: Text('Pop!'),
+          child: const Text('Pop!'),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
       ),
-=======
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _widgetOptions = <Widget>[
-    const Text(
-      'First Screen',
-      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-    ),
-    const Text(
-      'Second Screen',
-      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-    ),
-    const Text(
-      'Third Screen',
-      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-    ),
-    const Text(
-      'Fourth Screen',
-      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-              bottom: PreferredSize(
-                preferredSize: const Size.fromHeight(48.0),
-                child: Theme(
-                  data: Theme.of(context).copyWith(),
-                  child: Container(
-                      height: 48.0,
-                      alignment: Alignment.center,
-                      child: const Text('AppBar Bottom Text')),
-                ),
-              ),
-              flexibleSpace: Container(
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('images/big.jpeg'),
-                        fit: BoxFit.fill)),
-              ),
-              title: const Text('AppBar Title'),
-              actions: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.add_alert),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.phone),
-                  onPressed: () {},
-                ),
-              ]),
-          body: Center(
-            child: _widgetOptions.elementAt(_selectedIndex),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: const Icon(Icons.add),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            //fixed, shifting
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'First',
-                  backgroundColor: Colors.green),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.business),
-                  label: 'Second',
-                  backgroundColor: Colors.red),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.school),
-                  label: 'Third',
-                  backgroundColor: Colors.purple),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.school),
-                  label: 'Fourth',
-                  backgroundColor: Colors.pink)
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.amber[800],
-            onTap: _onItemTapped,
-          ),
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                const DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text('Drawer Header'),
-                ),
-                ListTile(
-                  title: const Text('Item 1'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: const Text('Item 2'),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          )),
->>>>>>> ade667518108ac374a0d4b5cdb43981abdd5856e
     );
   }
 }
