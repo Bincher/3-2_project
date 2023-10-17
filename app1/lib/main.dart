3ef1ecf9bdf3c85aa2a66fa44cbe7b47dc9cd6d3
seongbin <catsb21@gmail.com>
2023-10-17 13:28:27 +0900

    과제2 : Todo List 작성

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,108 +1,184 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Work List',
+      home: HomePage(),
+    );
   }
 }
 
-class MyAppState extends State<MyApp> {
-  String result = '';
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  onPressGet() async {
-    Map<String, String> headers = {
-      "content-type": "application/json",
-      "accept": "application/json",
-    };
-    http.Response response = await http.get(
-        Uri.parse('https://jsonplaceholder.typicode.com/posts/1'),
-        headers: headers);
-    if (response.statusCode == 200) {
-      setState(() {
-        result = response.body;
-      });
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Map<String, dynamic>> _workList = [
+    {"id": 1, "work": "10/17 소프트웨어공학 과제2", "isChecked": true},
+    {"id": 2, "work": "10/19 오픈소스프로젝트 과제2", "isChecked": false},
+    {"id": 3, "work": "10/30 과학기술영어독해 어휘과제1", "isChecked": false},
+  ];
+
+  List<Map<String, dynamic>> _foundWorks = [];
+  bool _showCompleted = true;
+
+  @override
+  initState() {
+    _foundWorks = List.from(_workList);
+    super.initState();
+  }
+
+  void _dialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        String newWork = "";
+        return AlertDialog(
+          title: const Text("할 일 입력"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                onChanged: (value) => newWork = value,
+                decoration: const InputDecoration(border: OutlineInputBorder()),
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Create a new work with the entered name
+                Map<String, dynamic> newWorkItem = {
+                  "id": _workList.length + 1,
+                  "work": newWork,
+                  "isChecked": false,
+                };
+
+                setState(() {
+                  // Add the new work to the list
+                  _workList.add(newWorkItem);
+                  _foundWorks.add(newWorkItem);
+                });
+
+                Navigator.of(context).pop();
+              },
+              child: const Text("OK"),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = List.from(_workList);
     } else {
-      print('error......');
+      results = _workList
+          .where((work) =>
+              work["work"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
     }
-  }
 
-  onPressPost() async {
-    try {
-      http.Response response = await http.post(
-          Uri.parse('https://jsonplaceholder.typicode.com/posts'),
-          body: {'title': 'hello', 'body': 'world', 'userId': '1'});
-      print('statusCode : ${response.statusCode}');
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        setState(() {
-          result = response.body;
-        });
-      } else {
-        print('error......');
-      }
-    } catch (e) {
-      print('error ... $e');
-    }
-  }
-
-  onPressClient() async {
-    var client = http.Client();
-    try {
-      http.Response response = await client.post(
-          Uri.parse('https://jsonplaceholder.typicode.com/posts'),
-          body: {'title': 'hello', 'body': 'world', 'userId': '1'});
-
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        response = await client
-            .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
-        setState(() {
-          result = response.body;
-        });
-      } else {
-        print('error......');
-      }
-    } finally {
-      client.close();
-    }
+    setState(() {
+      _foundWorks = results;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Test'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(result),
-              ElevatedButton(
-                onPressed: onPressGet,
-                child: const Text('GET'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: onPressPost,
-                child: const Text('POST'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: onPressClient,
-                child: const Text('Client'),
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Work List'),
+        actions: [
+          IconButton(
+            icon: Icon(_showCompleted ? Icons.check : Icons.clear),
+            onPressed: () {
+              setState(() {
+                _showCompleted = !_showCompleted;
+              });
+            },
           ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _dialog,
+              child: const Icon(Icons.add),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              onChanged: (value) => _runFilter(value),
+              decoration: const InputDecoration(
+                  labelText: 'Search', suffixIcon: Icon(Icons.search)),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: _foundWorks.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _foundWorks.length,
+                      itemBuilder: (context, index) {
+                        final work = _foundWorks[index];
+                        if (!_showCompleted && work["isChecked"]) {
+                          return SizedBox.shrink();
+                        }
+                        return Card(
+                            key: ValueKey(work["id"]),
+                            color: Colors.amberAccent,
+                            elevation: 4,
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: ListTile(
+                                leading: Checkbox(
+                                  value: work["isChecked"],
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      work["isChecked"] = newValue;
+                                    });
+                                  },
+                                ),
+                                title: Text(
+                                  work['work'],
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    decoration: work["isChecked"]
+                                        ? TextDecoration.lineThrough
+                                        : TextDecoration.none,
+                                  ),
+                                ),
+                                trailing: IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    onPressed: () {
+                                      setState(
+                                        () {
+                                          _foundWorks.remove(work);
+                                          _workList.remove(work);
+                                        },
+                                      );
+                                    })));
+                      })
+                  : const Text(
+                      'No results found',
+                      style: TextStyle(fontSize: 24),
+                    ),
+            ),
+          ],
         ),
       ),
     );
