ef78aad27e85c2dac4adbcfff6d3f56f65064353
seongbin <catsb21@gmail.com>
2023-10-19 11:34:37 +0900

    상위 위젯에서 상태 관리하기

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,16 +1,29 @@
-// ignore_for_file: use_key_in_widget_constructors
-
-import 'dart:async';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const ParentWidget());
+}
 
-class MyApp extends StatelessWidget {
-  Future<int> delay() {
-    return Future<int>(() async {
-      var msec = 2000;
-      await Future.delayed(Duration(milliseconds: msec));
-      return msec;
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  ParentWidgetState createState() => ParentWidgetState();
+}
+
+class ParentWidgetState extends State<ParentWidget> {
+  bool favorited = false;
+  int favoriteCount = 0;
+
+  void toggleFavorite() {
+    setState(() {
+      if (favorited) {
+        favoriteCount -= 1;
+        favorited = false;
+      } else {
+        favoriteCount += 1;
+        favorited = true;
+      }
     });
   }
 
@@ -19,39 +32,77 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Test'),
+          title: const Text('State Test'),
         ),
-        body: FutureBuilder(
-          future: delay(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Center(
-                child: defaultText('${snapshot.data}')
-              );
-            }
-            return const Center(
-              child: defaultText("wating"),
-            );
-          },
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Parent Favorate ${favorited.toString()}'),
+            Text('Parent Count ${favoriteCount.toString()}'),
+            IconWidget(favorited: favorited, onChanged: toggleFavorite),
+            ContentWidget(favoriteCount: favoriteCount),
+          ],
         ),
       ),
     );
   }
 }
 
-// ignore: camel_case_types
-class defaultText extends StatelessWidget {
-  final String string;
-  const defaultText(
-    this.string,
-    {super.key}
-  );
+class IconWidget extends StatelessWidget {
+  final bool favorited;
+  final Function onChanged;
+
+  const IconWidget(
+      {super.key, this.favorited = false, required this.onChanged});
+
+  void _handleTap() {
+    onChanged();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      string,
-      style: const TextStyle(color: Colors.black, fontSize: 30),
+    return Center(
+      child: IconButton(
+        icon: (favorited
+            ? const Icon(Icons.favorite)
+            : const Icon(Icons.favorite_border)),
+        iconSize: 200,
+        color: Colors.red,
+        onPressed: _handleTap,
+      ),
+    );
+  }
+}
+
+class ContentWidget extends StatefulWidget {
+  int favoriteCount;
+
+  ContentWidget({super.key, required this.favoriteCount});
+
+  @override
+  State<ContentWidget> createState() => _ContentWidgetState();
+}
+
+class _ContentWidgetState extends State<ContentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+            child: Text( // widget -> ContentWidget class를 전달 받기 위함
+              'Child favoriteCount : ${widget.favoriteCount}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                widget.favoriteCount += 1;
+              });
+            }),
+      ],
     );
   }
 }
