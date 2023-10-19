9679630986ed834861471214b1c9a6e0f346c9b4
seongbin <catsb21@gmail.com>
2023-10-19 10:28:09 +0900

    Future 활용하기

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,1 +1,48 @@
+// ignore_for_file: use_key_in_widget_constructors
 
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Future<int> delay() {
+    return Future<int>(() async {
+      var msec = 2000;
+      await Future.delayed(Duration(milliseconds: msec));
+      return msec;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Test'),
+        ),
+        body: FutureBuilder(
+          future: delay(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                child: Text(
+                  '${snapshot.data}',
+                  style: const TextStyle(color: Colors.black, fontSize: 30),
+                ),
+              );
+            }
+            return const Center(
+              child: Text(
+                'waiting',
+                style: TextStyle(color: Colors.black, fontSize: 30),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
