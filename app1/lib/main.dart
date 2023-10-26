4e473eeb59af48f888d4cd7ffba778c0c9819ca9
seongbin <catsb21@gmail.com>
2023-10-26 10:25:14 +0900

    프로젝트를 위한 크롤링시도

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,62 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> menuLines = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMenuData();
+  }
+
+  Future<void> fetchMenuData() async {
+    final response = await http.get(Uri.parse("https://www.kumoh.ac.kr/ko/restaurant01.do"));
+
+    final document = parse(response.body);
+
+    // 메뉴 정보를 가져오는 CSS 선택자를 지정합니다.
+    final menuElement = document.querySelector(".menu-list-box table tbody tr:nth-child(1) td:nth-child(1)");
+
+    if (menuElement != null) {
+      // 선택된 요소에서 텍스트를 추출합니다.
+      final menuText = menuElement.text;
+
+      // 텍스트를 문장으로 분할하고 리스트에 추가합니다.
+      final lines = menuText.split('\n');
+      setState(() {
+        menuLines = lines;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Provider Test'),
+          title: Text(menuLines[13]),
         ),
-        body: Provider<int>(
-          create: (context) {
-            int sum = 0;
-            for (int i = 1; i <= 10; i++) {
-              sum += i;
-            }
-            return sum;
-          },
-          child: const SubWidget(),
+        body: Center(
+          child: Column(
+            children: menuLines.map((line) => Text(line)).toList(),
+          ),
+          
         ),
       ),
     );
   }
 }
-
-class SubWidget extends StatelessWidget {
-  const SubWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final data = Provider.of<int>(context);
-    return Container(
-      color: Colors.orange,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('I am SubWidget',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-            Text(
-              'Provider Data : $data',
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
