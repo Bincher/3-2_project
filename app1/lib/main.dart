7808876931de4fc02474ec499d17e130e23404a4
seongbin <catsb21@gmail.com>
2023-11-06 11:48:05 +0900

    바텀네비게이션바 임시 저장

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,61 +1,119 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' show parse;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> menuLines = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchMenuData();
-  }
-
-  Future<void> fetchMenuData() async {
-    final response = await http.get(Uri.parse("https://www.kumoh.ac.kr/ko/restaurant01.do"));
-
-    final document = parse(response.body);
-
-    // 메뉴 정보를 가져오는 CSS 선택자를 지정합니다.
-    final menuElement = document.querySelector(".menu-list-box table tbody tr:nth-child(1) td:nth-child(1)");
-
-    if (menuElement != null) {
-      // 선택된 요소에서 텍스트를 추출합니다.
-      final menuText = menuElement.text;
-
-      // 텍스트를 문장으로 분할하고 리스트에 추가합니다.
-      final lines = menuText.split('\n');
-      setState(() {
-        menuLines = lines;
-      });
-    }
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(menuLines[13]),
-        ),
-        body: Center(
-          child: Column(
-            children: menuLines.map((line) => Text(line)).toList(),
-          ),
-          
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Alarm Setting',
+      home: AlarmPage(),
     );
   }
 }
+
+class AlarmPage extends StatefulWidget {
+  const AlarmPage({Key? key}) : super(key: key);
+
+  @override
+  State<AlarmPage> createState() => _AlarmPageState();
+}
+class _AlarmPageState extends State<AlarmPage> {
+  @override
+  Widget build(BuildContext context) {
+    int _selectedIndex = 0;
+
+    final List<Widget> _widgetOptions = <Widget>[
+      AlarmPage(),
+    //MySellPostPage(),
+    //MyPostPage(),
+    //MyPage(),
+    ];
+
+    void _onItemTapped(int index) { // 탭을 클릭했을떄 지정한 페이지로 이동
+        setState(() {
+          _selectedIndex = index;
+        });
+      }
+
+    final controller = TextEditingController();
+    int textCounter = 0;
+
+    _printValue() {
+      print("_printValue(): ${controller.text}");
+      setState(() {
+        textCounter = controller.text.length;
+      });
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      controller.addListener(_printValue);
+    }
+
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Work List'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(1),
+        child: Column(
+          children: [
+            _widgetOptions.elementAt(_selectedIndex),
+            const Text('TextField Test'),
+            TextField(
+              style: const TextStyle(fontSize: 15.0),
+              controller: controller,
+              decoration: InputDecoration(
+                labelText: 'Data',
+                prefixIcon: const Icon(Icons.input),
+                border: const OutlineInputBorder(),
+                hintText: "Hint Text",
+                helperText: "데이터를 입력하세요.",
+                counterText: "$textCounter characters",
+              ),
+              textInputAction: TextInputAction.search,
+              keyboardType: TextInputType.emailAddress,
+              minLines: 5,
+              maxLines: 5,
+              ),
+          ]
+        )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Calender'
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Alarm'
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'map'
+            ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+        )
+    );
+  }
+}
+
+  
